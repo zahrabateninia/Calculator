1316217c77aa6ddbb24d5f12c183299d9fc2e3f9
zahrabateninia <zahrabatenin@gmail.com>
2023-05-25 00:47:39 +0330

    buttons, basic math operator functions are created

---
+++ calculator.js
@@ -0,0 +1,21 @@
+function sum(num1,num2){
+    let sum = num1 + num2;
+    return sum;
+};
+function divide(num1,num2){
+    let divide = num1/num2;
+    return divide;
+};
+function minus(num1,num2){
+    let minus = num1 - num2;
+    return minus;
+}
+function multiply(num1,num2){
+    let multiply = num1 * num2;
+    return multiply;
+}
+//takes the num1 and num2 and the operator:
+
+//...implement the code
+//call the right function
+
